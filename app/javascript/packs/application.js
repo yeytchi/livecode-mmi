8c8d33091b99f02f310feb6bc45eb80f1b58ee35
nicolas beaudouin <nicolas.pn.beaudouin@gmail.com>
2019-09-18 11:06:04 +0200

    repo working on html, css and js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,17 @@
 import "bootstrap";
+
+const button = document.querySelector('.btn-success');
+const author = document.querySelector('.author-avatar');
+
+button.addEventListener('click', (e) => {
+  button.classList.add('disabled');
+  button.innerText = 'Déjà abonné';
+
+  author.insertAdjacentHTML('beforeend', `<i class="fas fa-heart red"></i>`);
+});
+
+const input = document.querySelector('#input');
+const title = document.querySelector('h1');
+input.addEventListener('keyup', (e) => {
+  title.innerText = input.value;
+});
